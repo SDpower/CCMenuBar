4dcdf01eb5b2af1c474dff308a9ead164c43d303
Steve Lo <info@sd.idv.tw>
2026-03-17 20:42:26 +0800

    Initial Commit

---
+++ CCMenuBar/CCMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCMenuBarApp.swift
+//  CCMenuBar
+//
+//  Created by SteveLo on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct CCMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCMenuBarTests/CCMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  CCMenuBarTests.swift
+//  CCMenuBarTests
+//
+//  Created by SteveLo on 2026/3/17.
+//
+
+import Testing
+@testable import CCMenuBar
+
+struct CCMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
